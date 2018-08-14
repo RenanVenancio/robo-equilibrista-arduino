5e99be68503f3b8ed1c9dc4700cb9517ba6aded6
Renan Venâncio <renan_1419@hotmail.com>
2018-08-14 07:23:39 -0300

    Otimizando o codigo

--- ROBO2018_3/ROBO2018_3.ino
+++ ROBO2018_3/ROBO2018_3.ino
@@ -48,31 +48,7 @@ int pot2    = 0;
 int angulo1 = 0;
 int angulo2 = 0;
 
-*/
 
-
-/* Copyright (C) 2012 Kristian Lauszus, TKJ Electronics. All rights reserved.
- This software may be distributed and modified under the terms of the GNU
- General Public License version 2 (GPL2) as published by the Free Software
- Foundation and appearing in the file GPL2.TXT included in the packaging of
- this file. Please note that GPL2 Section 2[b] requires that all works based
- on this software must also be made publicly available under the terms of
- the GPL2 ("Copyleft").
- Contact information
- -------------------
- Kristian Lauszus, TKJ Electronics
- Web      :  http://www.tkjelectronics.com
- e-mail   :  kristianl@tkjelectronics.com
- */
-
-
-#include <Wire.h>
-#include <Kalman.h> // Source: https://github.com/TKJElectronics/KalmanFilter
-
-#define RESTRICT_PITCH // Comment out to restrict roll to ±90deg instead - please read: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf
-
-Kalman kalmanX; // Create the Kalman instances
-Kalman kalmanY;
 
 /* IMU Data */
 double accX, accY, accZ;
